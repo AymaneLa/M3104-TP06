e112ca216d310786b27b0378b5a452ffeba60f54
LARAICHI Aymane <laraicha@hostname.iut2.upmf-grenoble.fr>
2019-10-22 11:41:55 +0200

    fgjfg

---
+++ tp06/View/click.view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Informations</title>
+    <link rel="stylesheet" href="Design/clickView.css">
+  </head>
+  <body>
+    <header>
+      <h1>MobiFind</h1>
+      <img src="Design/logo.png" alt="">
+    </header>
+
+    <article class="">
+      <h2>ONEPLUS 7T Pro</h2>
+      <h3>Prix</h3>
+      <h3>Description</h3>
+      <h3>Adresse</h3>
+      <h3>Disponibilité</h3>
+      <img src="../data/images/ONEPLUS 7T Pro.jpeg" alt="">
+    </article>
+
+  </body>
+</html>
