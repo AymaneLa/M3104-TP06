e6a77a3b293447116a0c0d8998b598d7869d388f
LARAICHI Aymane <laraicha@hostname.iut2.upmf-grenoble.fr>
2019-10-25 14:01:37 +0200

    rueure

--- tp06/View/main.view.php
+++ tp06/View/main.view.php
@@ -27,7 +27,6 @@
 
         print(" <a href=\"http://www-etu-info.iut2.upmf-grenoble.fr/~laraicha/ProgWeb/tp06/tp06/controler/click.controler.php?ID=". $id->getId()."\">" );
         ?>
-<<<<<<< HEAD
         <div class="">
           <h2>"<?=$id->getMarque()?>"</h2>
           <img src="<?=$id->getImage() ?>"   alt=""></a>
@@ -36,13 +35,6 @@
 
 
         <?php  } ?>
-=======
-        <img src="<?=$id->getImage() ?>"   alt=""></a>
-        <h3><?= $id->getPrix() ?> € </h3>
-
-        <?php  } ?>
-
->>>>>>> 1f28d7335920b397cd658c9c91f8f79ca8b4f19b
       <section>
         <p>Un telephone</p>
       </section>
