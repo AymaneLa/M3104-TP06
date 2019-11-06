16b2111f6cf1af8db951531110d170b797d6d251
LARAICHI Aymane <laraicha@hostname.iut2.upmf-grenoble.fr>
2019-11-06 16:30:30 +0100

    rtuirir

--- tp06/View/click.view.php
+++ tp06/View/click.view.php
@@ -21,8 +21,8 @@
       print("<img src=\"".$this->mobil->getImage()."\" alt=\"\">");
       print("<h3>".$this->mobil->getPrix()."</h3>");
       print("<h3>Description : "."</h3>"."<p>".$this->mobil->getDescription()."</p>");
-      print("<h3>".$this->mobil->getAdress()."</h3>");
-      print("<h3>".$this->mobil->getDisponibilite()."</h3>");
+      print("<h3>Adresse : "."</h3>".$this->mobil->getAdress()."</h3>");
+      print("<h3>Dispoibilité : "."</h3>".$this->mobil->getDisponibilite()."</h3>");
       ?>
     </article>
   </body>
